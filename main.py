287b4e593cfb09d209314d0d4c26a71c3774c91a
Skye725 <101329664+Skye725@users.noreply.github.com>
2023-08-17 20:28:23 +0100

    Update main.py
    监听port变化！

--- main.py
+++ main.py
@@ -252,4 +252,4 @@ def standardize(data, save_csv=False, file_path=""):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
