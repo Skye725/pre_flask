1973202ea778499ce028ef075c62923614313571
Skye725 <101329664+Skye725@users.noreply.github.com>
2023-08-22 03:33:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -250,4 +250,4 @@ def standardize(data, save_csv=False, file_path=""):
     return data_scaled
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
